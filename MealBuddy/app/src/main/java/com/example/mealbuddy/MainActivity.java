4352b7b4a7aa59f771bc44239e2a2af9fb50f027
Tim Reeve <twreeve@syr.edu>
2017-02-26 12:24:17 -0500

    Add AppBar to MainActivity

--- MealBuddy/app/src/main/java/com/example/mealbuddy/MainActivity.java
+++ MealBuddy/app/src/main/java/com/example/mealbuddy/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.mealbuddy;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,6 +15,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set app toolbar
+        Toolbar app_toolbar = (Toolbar) findViewById(R.id.app_toolbar);
+        setSupportActionBar(app_toolbar);
 
         Button logout = (Button) findViewById(R.id.logout_button);
         logout.setOnClickListener(new View.OnClickListener() {

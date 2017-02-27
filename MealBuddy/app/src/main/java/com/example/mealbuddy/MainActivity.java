8d3dadef303b86b7e0772143670f1c651e4d48a2
Tim Reeve <twreeve@syr.edu>
2017-02-26 22:24:18 -0500

    Add navigation drawer

--- MealBuddy/app/src/main/java/com/example/mealbuddy/MainActivity.java
+++ MealBuddy/app/src/main/java/com/example/mealbuddy/MainActivity.java
@@ -1,15 +1,26 @@
 package com.example.mealbuddy;
 
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String[] mDrawerItemLabels;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +31,33 @@ public class MainActivity extends AppCompatActivity {
         Toolbar app_toolbar = (Toolbar) findViewById(R.id.app_toolbar);
         setSupportActionBar(app_toolbar);
 
+        // Setup nav drawer
+        mDrawerItemLabels = new String[] { "Item 1", "Item 2", "Item 3" };
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.nav_drawer);
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                mDrawerLayout,
+                app_toolbar,
+                R.string.drawer_open,
+                R.string.drawer_close) {
+
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+            }
+
+            public void onDrawerOpened(View view) {
+                super.onDrawerOpened(view);
+            }
+        };
+
+        mDrawerLayout.addDrawerListener(mDrawerToggle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        mDrawerList.setAdapter(new ArrayAdapter<>(this, R.layout.nav_drawer_item,
+                mDrawerItemLabels));
+
         Button logout = (Button) findViewById(R.id.logout_button);
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,5 +66,19 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, LoginActivity.class));
             }
         });
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        mDrawerToggle.onConfigurationChanged(newConfig);
     }
 }
